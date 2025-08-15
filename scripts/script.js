57467f9da090544b3703b25c1efc7dbcd9693625
Rashid Hussain <r.hussain.29152@khi.iba.edu.pk>
2025-08-15 16:01:11 +0500

    Add dynamic year to footer copyright

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+/***************************************/
+/* Add dynmic year to footer copyright */
+/***************************************/
+
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEL.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
